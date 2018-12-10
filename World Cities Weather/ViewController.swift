708d1e6c1b72aee32ad81e8eb0533ec17beab260
Kalen Hammann <kalen7@gmail.com>
2018-12-10 12:18:08 -0500

    got data retrieval from weather-forecast.com working and fixed outlet bug

--- World Cities Weather/ViewController.swift
+++ World Cities Weather/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    @IBAction func getWeatherButton(_ sender: UIButton) {
+        let desiredCity = NSString(string: cityTextField.text ?? "X").replacingOccurrences(of: " ", with: "-")
+        if let url = URL(string: "https://www.weather-forecast.com/locations/" + desiredCity + "/forecasts/latest") {
+            let request = NSMutableURLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                if let error = error {
+                    print(error)
+                    DispatchQueue.main.sync {
+                        print("The weather there couldn't be found. Please try again")
+                    }
+                } else {
+                    if let unwrappedData = data {
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        print(dataString ?? "Didn't work!")
+                        DispatchQueue.main.sync {
+                            print("You want the weather for \(desiredCity), is that right?")
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

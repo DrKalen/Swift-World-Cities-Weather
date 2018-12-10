76734fa9a368a6321711d09e091de633992f049b
Kalen Hammann <kalen7@gmail.com>
2018-12-10 09:27:23 -0500

    initial commit

---
+++ World Cities Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  World Cities Weather
+//
+//  Created by Kalen Hammann on 12/10/18.
+//  Copyright © 2018 Human Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
